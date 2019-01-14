67a24bebf4d86381c80f4e2cbadcd750eaa216ed
Matt Dorn <matt.dorn@gmail.com>
2019-01-13 18:47:42 -0600

    Fix some eslint complaints

--- src/client/components/Main.jsx
+++ src/client/components/Main.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react';
 
 import Home from './Home';
@@ -10,31 +10,40 @@ import Login from './auth/Login';
 import Dashboard from './Dashboard';
 import Promos from './Promos';
 
-function onAuthRequired({history}) {
+function onAuthRequired({ history }) {
   history.push('/login');
 }
 
 const Main = () => (
   <Router>
     <div>
-        <Security
-          issuer={`${process.env.OKTA_URL}/oauth2/${process.env.AUTH_SERVER_ID}`}
-          client_id={process.env.OIDC_CLIENT_ID}
-          redirect_uri={window.location.origin + '/implicit/callback'}
-          onAuthRequired={onAuthRequired}
-          scope={['openid', 'email', 'profile', 'promos:read']}
-        >
-          <Nav />
-          <header className="masthead">
-            <div className="container h-100">
-              <Route path="/" exact={true} component={Home} />
-              <Route path="/promos" component={Promos} />
-              <SecureRoute path="/dashboard" component={Dashboard} />
-              <Route path="/login" render={() => <Login baseUrl={process.env.OKTA_URL} idpRequestContext={process.env.IDP_REQUEST_CONTEXT} />} />
-              <Route path="/implicit/callback" component={ImplicitCallback} />
-            </div>
-          </header>
-        </Security>
+      <Security
+        issuer={`${process.env.OKTA_URL}/oauth2/${process.env.AUTH_SERVER_ID}`}
+        client_id={process.env.OIDC_CLIENT_ID}
+        redirect_uri={`${window.location.origin}/implicit/callback`}
+        onAuthRequired={onAuthRequired}
+        scope={['openid', 'email', 'profile', 'promos:read']}
+      >
+        <Nav />
+        <header className="masthead">
+          <div className="container h-100">
+            <Route path="/" exact component={Home} />
+            <Route path="/promos" component={Promos} />
+            <SecureRoute path="/dashboard" component={Dashboard} />
+            <Route
+              path="/login"
+              render={
+                () => (<Login
+                  baseUrl={process.env.OKTA_URL}
+                  multiLogin={process.env.MULTI_LOGIN}
+                  idpRequestContext={process.env.IDP_REQUEST_CONTEXT}
+                />)
+              }
+              />
+            <Route path="/implicit/callback" component={ImplicitCallback} />
+          </div>
+        </header>
+      </Security>
       <Social />
       <Footer />
     </div>
--- src/client/components/auth/Login.jsx
+++ src/client/components/auth/Login.jsx
@@ -1,31 +1,21 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Redirect } from 'react-router-dom';
 import { withAuth } from '@okta/okta-react';
-import { TabContent, TabPane, Nav, NavItem, NavLink, Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
-import classnames from 'classnames';
 
 import { handleIdpDiscovery } from '../../util';
 import Wrapper from '../layout/Wrapper';
 import OktaSignInWidget from './OktaSignInWidget';
 
-export default withAuth(class Login extends Component {
+class Login extends Component {
   constructor(props) {
     super(props);
     this.onSuccess = this.onSuccess.bind(this);
     this.onError = this.onError.bind(this);
-    this.toggle = this.toggle.bind(this);
     this.state = {
       authenticated: null,
-      activeTab: '1'
     };
     this.checkAuthentication();
-  }
-
-  async checkAuthentication() {
-    const authenticated = await this.props.auth.isAuthenticated();
-    if (authenticated !== this.state.authenticated) {
-      this.setState({ authenticated });
-    }
   }
 
   componentDidUpdate() {
@@ -36,47 +26,61 @@ export default withAuth(class Login extends Component {
     if (res.status === 'SUCCESS') {
       return this.props.auth.redirect({ sessionToken: res.session.token });
     } else if (res.status === 'IDP_DISCOVERY') {
-      const idpInfo = handleIdpDiscovery(this.props.auth._config);
+      const idpInfo = handleIdpDiscovery(this.props.auth._config); // eslint-disable-line
       document.cookie = idpInfo.cookie;
       res.idpDiscovery.redirectToIdp(idpInfo.authUrl);
-      return;
     }
+    return null;
   }
 
-  onError(err) {
+  onError(err) {  // eslint-disable-line
     console.log('error logging in', err);
   }
 
-  toggle(tab) {
-    if (this.state.activeTab !== tab) {
-      this.setState({
-        activeTab: tab
-      });
+  async checkAuthentication() {
+    const authenticated = await this.props.auth.isAuthenticated();
+    if (authenticated !== this.state.authenticated) {
+      this.setState({ authenticated });
     }
   }
 
   render() {
     if (this.state.authenticated === null) return null;
     return this.state.authenticated ?
-      <Redirect to={{ pathname: '/' }}/> :
+      <Redirect to={{ pathname: '/' }} /> :
       <Wrapper>
         <div id="left">
           <OktaSignInWidget
             baseUrl={this.props.baseUrl}
-            idpRequestContext={this.props.idpRequestContext}
             onSuccess={this.onSuccess}
             onError={this.onError}
+            idpRequestContext={this.props.idpRequestContext}
           />
         </div>
         <div id="right">
           <div className="alert alert-info" role="alert">
-            <p><i className="fa fa-info-circle"></i>&nbsp;
-              This is an example of the <a href="https://developer.okta.com/code/javascript/okta_sign-in_widget">Okta Sign-In Widget</a>.
-              You may have arrived here after trying to access a link in the application that requires authentication, and is therefore
-              protected by the <code>SecureRoute</code> component provided by the <a href="https://github.com/okta/okta-oidc-js/tree/master/packages/okta-react">Okta React SDK</a>.
+            <p><i className="fa fa-info-circle" />&nbsp;
+              This is an example of the
+              <a href="https://developer.okta.com/code/javascript/okta_sign-in_widget">Okta Sign-In Widget</a>.
+              You may have arrived here after trying to access a link in the application that
+              requires authentication, and is therefore
+              protected by the <code>SecureRoute</code> component provided by the
+              <a href="https://github.com/okta/okta-oidc-js/tree/master/packages/okta-react">Okta React SDK</a>.
             </p>
           </div>
         </div>
       </Wrapper>;
   }
-});
+}
+
+Login.defaultProps = {
+  idpRequestContext: null,
+};
+
+Login.propTypes = {
+  auth: PropTypes.shape.isRequired,
+  baseUrl: PropTypes.string.isRequired,
+  idpRequestContext: PropTypes.string,
+};
+
+export default withAuth(Login);
